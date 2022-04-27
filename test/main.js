7b665ea9784e85c3713004d358efe0168c7d8977
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-04-27 17:42:50 +0100

    Added Bitmoji copyright notice

--- test/main.js
+++ test/main.js
@@ -236,3 +236,10 @@ window.onscroll = () => { //I tried to use the "scroll" event listener instead o
 		if (value.method.apply(this, value.args)) //'.apply()' unpacks the list of arguments required for this animation
 			animationsCollection.deleteAnimation(key); //if this animation has begun (the animations' method returned 'true') then it is time to stop the animation from pending
 }
+
+miscellaneous = {
+	copyrightNotice: () => {
+		if (window.confirm('Bitmoji avatars are copyright (c) protected by the Bitmoji organisation. The owners have deemed it legal to reuse their artwork for non-commercial purposes. Would you like to view this information in their guidelines?')) 
+			window.location.href='https://www.bitmoji.com/bitmoji_brand_guidelines.pdf#page=4';
+	}
+}
