6426b6dc70d824829862060573fa5634ba1c8ed1
Taylor <taylorcourtney2000@googlemail.com>
2022-07-03 16:17:09 +0100

    Fixed missing import error

--- security.py
+++ security.py
@@ -1,9 +1,17 @@
-from werkzeug.security import safe_str_cmp
+from hmac import compare_digest
 from models.user import UserModel
 
 def authenticate(username, password): # checks if the user entered the correct password for a username
     user = UserModel.find_by_username(username)
-    if user and safe_str_cmp(user.password, password): # 'safe_str_cmp' is used to compare different character encodings simply: comparing ASCII with Unicode will cause issues
+
+     # convert string encodings to the same format: comparing ASCII with Unicode will cause issues
+    stored_password, password_provided = user.password, password
+    if isinstance(stored_password, str):
+        stored_password = stored_password.encode("utf-8")
+    if isinstance(password_provided, str):
+        password_provided = password_provided.encode("utf-8")
+
+    if user and compare_digest(stored_password, password_provided):
         return user
 
 def identity(payload): # returns the user details, acquired by ID
