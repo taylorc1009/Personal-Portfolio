c4297219ce94793552f601c543bf928a10296d63
Taylor <taylorcourtney2000@googlemail.com>
2022-12-01 14:26:37 +0000

    Deactivated database

--- app.py
+++ app.py
@@ -10,20 +10,18 @@ from resources.user import UserRegister
 from datetime import timedelta
 from db import db
 
-api_necessary = False # this is only used when I don't want the API to run in deployment, for example: during design periods an early stages
-
 app = Flask(__name__)
-app.secret_key = 'example-key'
+"""app.secret_key = 'example-key'
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # try to load the 'DATABASE_URL' variable, if it fails then revert back to the SQLite implementation
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # disables Flasks' native tracker but not SQLAlchemys': tracking twice would use more resources
 
-db.init_app(app)
+db.init_app(app)"""
 api = Api(app)
 
-@app.before_first_request # stop any app requests being made before this method is ran
+"""@app.before_first_request # stop any app requests being made before this method is ran
 def create_tables():
 	db.create_all(); # SQLAlchemy will only create the tables it sees, for example: 'app.py' imports 'resources.store' and 'resources.store' imports 'models.store', this is where it finds the 'stores' table as it is in 'StoreModel' of 'store.py'
 
@@ -34,13 +32,13 @@ def customized_response_handler(access_token, identity):
 	return jsonify({
 		'access_token': access_token.decode('utf-8'),
 		'user_id': identity.id
-	})
+	})"""
 
 @app.route("/")
 def home():
 	return render_template("index.html")
 
-api.add_resource(UserRegister, '/register')
+""""api.add_resource(UserRegister, '/register')
 
-if __name__ == '__main__' and api_necessary:
-	app.run(port=5000, debug=True)
+if __name__ == '__main__': # this is not needed in deployment
+	app.run(port=5000, debug=True)"""
