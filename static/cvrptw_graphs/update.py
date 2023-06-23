f6ad72a955191740e3e396c0ed7da70696889776
Taylor <taylorcourtney2000@googlemail.com>
2023-06-23 15:28:49 +0100

    Fixed hexes' update.py

--- static/cvrptw_graphs/update.py
+++ static/cvrptw_graphs/update.py
@@ -5,13 +5,8 @@
 import os, re
 
 replacements = {
-    "#264478": "#C31432",
-    "#A5A5A5": "#D5D5D5",
-    "#255E91": "#6bE585",
-    "#4472C4": "#8360C3",
-    "#43682B": "#78FFDB",
-    "#9E480E": "#FF4B2B",
-    "#997300": "#FFFDE4"
+    "#636363": "#F2727F",
+    "#6bE585": "#6BE585",
 }
 
 for filename in [file for file in os.listdir(os.getcwd()) if file.endswith(".svg")]:
@@ -20,17 +15,15 @@ for filename in [file for file in os.listdir(os.getcwd()) if file.endswith(".svg
 
     updated_lines = []
     for line in lines:
-        try:
-            hex_values = re.findall(r'#[0-9A-Fa-f]{6}+', line)
+        hex_values = re.findall(r'#[0-9A-Fa-f]{6}', line)
 
-            for hex_value in hex_values:
-                if hex_value in replacements:
-                    line = line.replace(hex_value, replacements[hex_value])
-        except:
-            pass
+        for hex_value in hex_values:
+            if hex_value in replacements:
+                line = line.replace(hex_value, replacements[hex_value].upper())
 
         updated_lines.append(
-            re.sub(r'(?<!\n)<path', r'\n<path', line)
+            line
+            #re.sub(r'(?<!\n)<path', r'\n<path', line)
         )
 
     with open(os.path.join(os.getcwd(), filename), "w") as svg:
